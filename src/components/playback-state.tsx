7445fd9394b19d3dae31c9376a2120156331b581
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-01 14:51:19 +0000

    didnt wokr :(

--- src/components/playback-state.tsx
+++ src/components/playback-state.tsx
@@ -21,7 +21,6 @@ export function PlaybackState({
     data: UpdateEvent["data"],
 }) {
     const [progress, setProgress] = useState(data.interpolatedProgress ?? data.state?.progressNormal ?? 0);
-    const [imageLoaded, setImageLoaded] = useState(false);
 
     useEffect(() => {
         setProgress(data.interpolatedProgress ?? data.state?.progressNormal ?? 0);
@@ -29,16 +28,7 @@ export function PlaybackState({
 
     return (<>
         <HStack alignItems="self-start" width="100%">
-            <Box width="64px" height="64px" background="rgba(255, 255, 255, 0.2)" borderRadius="6px">
-                <Image
-                    width="64px"
-                    height="64px"
-                    borderRadius="6px"
-                    src={data.state?.imageUrl}
-                    onLoad={() => setImageLoaded(true)}
-                    style={{ display: imageLoaded ? 'block' : 'none' }}
-                />
-            </Box>
+            <Image width="64px" height="64px" background="rgba(255, 255, 255, 0.2)" borderRadius="6px" src={data.state?.imageUrl} />
             <Stack height="100%" width="100%" gap="0" fontFamily="arial, helvetica" lineHeight="18px">
                 <HStack pos="relative" gap="5px" justifyContent="space-between">
                     <HStack width="100%" gap="5px">
