c14928c14388851745ae6635ccf63ca62ee94992
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-02 10:38:57 +0000

    Update on replay cunt

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -357,7 +357,7 @@ export function UIApp({
                                     <Box width="100%" height="1px" background="rgba(255, 255, 255, 0.2)" />
                                 )}
                                 <Stack gap="8px">
-                                    <HStack key={"ui-" + v.userId} justifyContent="space-between">
+                                    <HStack key={"ui-" + v.userId + v.data.state?.replayCount} justifyContent="space-between">
                                         <HStack>
                                             {data.state?.pfpUrl !== "" && (
                                                 <Image width="24px" borderRadius="6px" src={data.state?.pfpUrl} />
