f008683a7272f42ae14f7dd48be77b7cf7b7e7eb
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-02 15:46:44 +0000

    try reconnect

--- src/lib/live-ingest.ts
+++ src/lib/live-ingest.ts
@@ -116,140 +116,155 @@ export class DataStreamer extends EventEmitter {
     async init(prevUserIds?: string[]) {
         this.cleanup();
 
-        try {
-            // Setup connection to server
-            let sessions = await this.fetchPublicStreams();
+        const retryInterval = 6e3;
+        const maxRetries = 50;
+        let attempts = 0;
 
-            if (this.interval)
-                try { clearInterval(this.interval); } catch { }
+        const connect = async () => {
+            try {
+                // Setup connection to server
+                let sessions = await this.fetchPublicStreams();
 
-            // Remove states if they no longer exist
-            // Such as if app is reopened after a while and states are now stale
-            for (const userId of prevUserIds ?? []) {
-                if (!sessions.includes(userId))
-                    this.emit("remove", userId);
-            }
+                if (this.interval)
+                    try { clearInterval(this.interval); } catch { }
 
-            this.sock = new WebSocket(API_URL_SOCK + "/stream/sessions");
+                // Remove states if they no longer exist
+                // Such as if app is reopened after a while and states are now stale
+                for (const userId of prevUserIds ?? []) {
+                    if (!sessions.includes(userId))
+                        this.emit("remove", userId);
+                }
 
-            this.interval = setInterval(async () => {
-                if (!this.sock || !this.sock.OPEN)
-                    return;
+                this.sock = new WebSocket(API_URL_SOCK + "/stream/sessions");
 
-                const newSessions = await this.fetchPublicStreams();
-                const currentListeners = await this.getListeners();
-
-                if (currentListeners.sort().join("") !== newSessions.join(""))
-                    this.sock.send(JSON.stringify(newSessions));
-            }, 5e3);
-
-            let userIntervals: {[key: string]: NodeJS.Timeout} = {};
-
-            this.sock.onmessage = (m) => {
-                try {
-                    const data = JSON.parse(m.data) as StateUpdateEvent;
-
-                    // This is a keepalive
-                    if (this.sock && data.code == -1) {
-                        this.sock.send(JSON.stringify({
-                            code: -1
-                        }));
-
+                this.interval = setInterval(async () => {
+                    if (!this.sock || !this.sock.OPEN)
                         return;
-                    }
 
-                    if (data.code == 200) {
-                        const state = data.data;
-                        const action = state?.action;
+                    const newSessions = await this.fetchPublicStreams();
+                    const currentListeners = await this.getListeners();
 
-                        let parsed: ParsedStateUpdate = {
-                            state: state?.state,
-                            action: {
-                                type: action as ParsedStateUpdate["action"]["type"] ?? "LOAD",
-                                songId: "",
-                            }
-                        };
+                    if (currentListeners.sort().join("") !== newSessions.join(""))
+                        this.sock.send(JSON.stringify(newSessions));
+                }, 5e3);
 
-                        const intervalId = (parsed.state?.userId ?? "");
+                let userIntervals: {[key: string]: NodeJS.Timeout} = {};
 
-                        if (userIntervals[intervalId])
-                            clearInterval(userIntervals[intervalId]);
+                this.sock.onmessage = (m) => {
+                    try {
+                        const data = JSON.parse(m.data) as StateUpdateEvent;
 
-                        if (action?.startsWith("PLAYING") || action?.startsWith("SKIPPED") || action?.startsWith("LISTENED") || action?.startsWith("PAUSED") || action?.startsWith("REPLAYED")) {
-                            const targetAction = action.split(":")[0];
-                            const songId = action.split(":")[1];
+                        // This is a keepalive
+                        if (this.sock && data.code == -1) {
+                            this.sock.send(JSON.stringify({
+                                code: -1
+                            }));
 
-                            parsed.action.type = targetAction as ParsedStateUpdate["action"]["type"];
-                            parsed.action.songId = songId;
+                            return;
                         }
 
-                        const userId = (parsed.state?.userId ?? "");
+                        if (data.code == 200) {
+                            const state = data.data;
+                            const action = state?.action;
 
-                        if (parsed.state?.isPlaying) {
-                            const now = Date.now();
-                            const timeElapsed = (now - parsed.state.updatedAt) / 1000; // in seconds
-                            const duration = parsed.state.duration;
-                            const currentTime = Math.min(duration * parsed.state.progressNormal, duration);
-                            parsed.interpolatedProgress = (currentTime + (timeElapsed * 1e3)) / duration;
+                            let parsed: ParsedStateUpdate = {
+                                state: state?.state,
+                                action: {
+                                    type: action as ParsedStateUpdate["action"]["type"] ?? "LOAD",
+                                    songId: "",
+                                }
+                            };
 
-                            if (intervalId !== "") {
-                                userIntervals[intervalId] = setInterval(() => {
-                                    requestAnimationFrame(() => {
-                                        const now = Date.now();
-                                        const timeElapsed = (now - parsed.state!.updatedAt) / 1000; // in seconds
-                                        const interpolatedValue = Math.min((currentTime + (timeElapsed * 1e3)) / duration, 1);
-                                        parsed.interpolatedProgress = interpolatedValue;
+                            const intervalId = (parsed.state?.userId ?? "");
 
-                                        const payload: UpdateEvent = {
-                                            userId,
-                                            data: parsed,
-                                        };
+                            if (userIntervals[intervalId])
+                                clearInterval(userIntervals[intervalId]);
 
-                                        if (parsed.state)
-                                            this.cache[userId] = payload;
+                            if (action?.startsWith("PLAYING") || action?.startsWith("SKIPPED") || action?.startsWith("LISTENED") || action?.startsWith("PAUSED") || action?.startsWith("REPLAYED")) {
+                                const targetAction = action.split(":")[0];
+                                const songId = action.split(":")[1];
 
-                                        this.emit("update", payload);
-                                        this.emit("update-" + userId , payload);
-                                    });
-                                }, 500);
+                                parsed.action.type = targetAction as ParsedStateUpdate["action"]["type"];
+                                parsed.action.songId = songId;
                             }
+
+                            const userId = (parsed.state?.userId ?? "");
+
+                            if (parsed.state?.isPlaying) {
+                                const now = Date.now();
+                                const timeElapsed = (now - parsed.state.updatedAt) / 1000; // in seconds
+                                const duration = parsed.state.duration;
+                                const currentTime = Math.min(duration * parsed.state.progressNormal, duration);
+                                parsed.interpolatedProgress = (currentTime + (timeElapsed * 1e3)) / duration;
+
+                                if (intervalId !== "") {
+                                    userIntervals[intervalId] = setInterval(() => {
+                                        requestAnimationFrame(() => {
+                                            const now = Date.now();
+                                            const timeElapsed = (now - parsed.state!.updatedAt) / 1000; // in seconds
+                                            const interpolatedValue = Math.min((currentTime + (timeElapsed * 1e3)) / duration, 1);
+                                            parsed.interpolatedProgress = interpolatedValue;
+
+                                            const payload: UpdateEvent = {
+                                                userId,
+                                                data: parsed,
+                                            };
+
+                                            if (parsed.state)
+                                                this.cache[userId] = payload;
+
+                                            this.emit("update", payload);
+                                            this.emit("update-" + userId , payload);
+                                        });
+                                    }, 500);
+                                }
+                            }
+
+                            const payload: UpdateEvent = {
+                                userId,
+                                data: parsed,
+                            };
+
+                            this.cache[userId] = payload;
+
+                            this.emit("update", payload);
+                            this.emit("update-" + userId, payload);
                         }
-
-                        const payload: UpdateEvent = {
-                            userId,
-                            data: parsed,
-                        };
-
-                        this.cache[userId] = payload;
-
-                        this.emit("update", payload);
-                        this.emit("update-" + userId, payload);
+                    } catch (ex) {
+                        console.warn("Failed to parse streaming API response, error:", ex);
                     }
-                } catch (ex) {
-                    console.warn("Failed to parse streaming API response, error:", ex);
+                }
+
+                this.sock.onclose = async () => {
+                    this.emit("close");
+                    
+                    Object.values(userIntervals).forEach(v => {
+                        try { clearInterval(v); } catch { }
+                    });
+
+                    await new Promise(resolve => setTimeout(resolve, 1e3));
+
+                    // Attempt to reconnect
+                    this.init();
+                }
+
+                this.sock.onopen = () => {
+                    if (this.sock)
+                        this.sock.send(JSON.stringify(sessions));
+                }
+            } catch (ex) {
+                console.error("Failed to initialise DataStreamer, error:", ex);
+                if (attempts < maxRetries) {
+                    attempts++;
+                    console.log(`Retrying connection (${attempts}/${maxRetries})...`);
+                    setTimeout(connect, retryInterval);
+                } else {
+                    window.location.reload()
                 }
             }
+        };
 
-            this.sock.onclose = async () => {
-                this.emit("close");
-                
-                Object.values(userIntervals).forEach(v => {
-                    try { clearInterval(v); } catch { }
-                });
-
-                await new Promise(resolve => setTimeout(resolve, 1e3));
-
-                // Attempt to reconnect
-                this.init();
-            }
-
-            this.sock.onopen = () => {
-                if (this.sock)
-                    this.sock.send(JSON.stringify(sessions));
-            }
-        } catch (ex) {
-            console.error("Failed to initialise DataStreamer, error:", ex);
-        }
+        connect();
     }
 
     public getPrevState(userId: string) {
