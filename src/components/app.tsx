64e7b304aec21d95c93351474b3c9154eb936b19
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-02 20:19:09 +0000

    Now Playin --? Latest

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -379,7 +379,7 @@ export function UIApp({
                         <Spinner size="lg" />
                     </Center>) : (
                         <Stack gap="18px" overflowY="auto" paddingBottom="18px">
-                            <Text fontFamily="arial, helvetica" fontWeight="bold" fontSize="24px">Now Playing</Text>
+                            <Text fontFamily="arial, helvetica" fontWeight="bold" fontSize="24px">Latest</Text>
                             {livePlaybackStates.map((v, i) => {
                                 // const key = v.userId + v.data.state?.songId + v.data.interpolatedProgress;
                                 const data = v.data;
