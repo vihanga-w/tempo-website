786a8165ae436d1a7d78e7dca70831a4c35ce38a
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-02 19:19:55 +0000

    Adjust gradient

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -265,12 +265,13 @@ export function UIApp({
 
             <Box
                 width="100vw"
-                height="50px"
+                height="115px"
                 pos="fixed"
                 top="0"
                 left="0"
-                background="linear-gradient(180deg, rgba(13,13,14,1) 10%, rgba(13,13,14,0) 100%)"
+                background="linear-gradient(180deg, rgba(13,13,14,0.82) 10%, rgba(13,13,14,0) 100%)"
                 zIndex="999"
+                pointerEvents="none"
             />
 
             <HStack
