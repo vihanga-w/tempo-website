48c371ea38101a0938120d8e22a9009abae323a2
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-14 10:46:10 +0000

    wrong location

--- src/components/playback-state.tsx
+++ src/components/playback-state.tsx
@@ -200,7 +200,7 @@ export function PlaybackState({
                                 src="/podcast-icon.svg"
                             />
                         )}
-                        <Image background="rgba(255, 255, 255, 0.2)" borderRadius="6px" src={data?.state?.imageUrl} draggable={false} />
+                        <Image width="72px" height="72px" background="rgba(255, 255, 255, 0.2)" borderRadius="6px" src={data?.state?.imageUrl} draggable={false} />
                     </Box>
                     <Stack height="100%" width="100%" gap="0" fontFamily="arial, helvetica" lineHeight="18px">
                         <HStack pos="relative" gap="5px" justifyContent="space-between">
