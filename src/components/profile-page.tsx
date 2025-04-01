27d50649ce8da27c26ae74def073d023cb9a1cfd
Vihanga Weerasinghe <vihanga1432@gmail.com>
2025-04-01 20:38:45 +0100

    so that does help

--- src/components/profile-page.tsx
+++ src/components/profile-page.tsx
@@ -590,9 +590,9 @@ export default function ProfilePage({
                         <Box
                             width="100%"
                             height="100%"
-                            // pos="absolute"
-                            // top="0"
-                            // left="0"
+                            pos="absolute"
+                            top="0"
+                            left="0"
                         >
                             <Center height="100%">
                                 <Spinner size="lg" color={reactiveDesignComplementaryColour ?? "#ffffff"} />
