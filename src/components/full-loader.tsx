90dd92f96bb795da075f6614574cda6895357ff2
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-04-20 03:16:12 +0100

    adjust delay

--- src/components/full-loader.tsx
+++ src/components/full-loader.tsx
@@ -86,7 +86,7 @@ export default function FullLoader() {
 
                 return newIndex;
             });
-        }, 180);
+        }, 200);
 
         return () => clearInterval(intervalId);
     }, []);
