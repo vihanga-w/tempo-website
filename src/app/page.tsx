5cb6f3030a9d344e26b5a1a2254c9ef3afc70745
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-05-17 17:33:32 +0100

    Add service status embed on perf notice page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -676,6 +676,12 @@ export default function Home() {
                 opacity="0.85"
               >{perfMsg}</Text>
             </Stack>
+            <Box pos="absolute" bottom="20px" left="0" right="0" margin="auto" width="220px" padding="4px" paddingLeft="6px" background="rgba(255, 255, 255, 0.075)" border="2px solid rgba(255, 255, 255, 0.05)" borderRadius="30px">
+              <iframe src="https://status.tempo-music.co/badge?theme=dark" width="220" height="30" style={{
+                background: "transparent",
+                colorScheme: "normal",
+              }} />
+            </Box>
           </Center>
       </>)}
     </Box>
